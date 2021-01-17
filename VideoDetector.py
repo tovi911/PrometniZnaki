11de65004ccdc66e725fd4fb3bc667d14f58e74c
nejc-tovornik <nejc.tovornik@student.um.si>
2021-01-17 19:01:06 +0100

    ImCrop

--- VideoDetector.py
+++ VideoDetector.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import argparse
-from Training.src.keras_yolo3.yolo import YOLO
+from Training.src.keras_yolo3.yolo import YOLO, detect_video
 from PIL import Image
 from timeit import default_timer as timer
 from Utils.utils import load_extractor_model, load_features, parse_input, detect_object
@@ -72,52 +72,11 @@ input_labels = [line.rstrip("\n") for line in class_file.readlines()]
 print("Found {} input labels: {} ...".format(len(input_labels), input_labels))
 
 # Yolo prepoznava
-if input_image_paths:
-    print("Found {} input images: {} ...".format(len(input_image_paths),[os.path.basename(f) for f in input_image_paths[:5]],))
+if input_video_paths:
+    print("Found {} input videos: {} ...".format(len(input_video_paths),[os.path.basename(f) for f in input_video_paths[:5]],))
     start = timer()
-    text_out = ""
-    # Zanka za izvajanje detekcije na vseh slikah
-    for i, img_path in enumerate(input_image_paths):
-        img_name = os.path.basename(img_path.rstrip("\n"))
-
-        # Naredi predikcije na trenutni sliki (save_img=1 označi in shrani sliko v output folder!)
-        prediction, image = detect_object(yolo,img_path,save_img=0,save_img_path=detection_results_folder)
-        y_size, x_size, _ = np.array(image).shape
-
-        try:
-            image = Image.open(img_path)
-            if image.mode != "RGB":
-                image = image.convert("RGB")
-            image_array = np.array(image)
-        except:
-            print("File Open Error! Try again!")
-
-        print("Na sliki {} je bilo zaznanih {} prometnih znakov\n".format(img_name, len(prediction)))
-
-        # Zanka za ustvarjanje datafraima za vsako predikcijo na trenutni sliki
-        for single_prediction in prediction:   
-            out_df = out_df.append(pd.DataFrame([[img_name, img_path.rstrip("\n"),]
-                        + single_prediction
-                        + [x_size, y_size]
-                    ],
-                    columns=[
-                        "image",
-                        "image_path",
-                        "xmin",
-                        "ymin",
-                        "xmax",
-                        "ymax",
-                        "label",
-                        "confidence",
-                        "x_size",
-                        "y_size",
-                    ],))
-            if single_prediction[5] > yolo_tresh:
-                cimage  =  image.crop((single_prediction[0], single_prediction[1], single_prediction[2], single_prediction[3]))
-                cimage.show()
-
+    for i, vid_path in enumerate(input_video_paths):
+        output_path = os.path.join(detection_results_folder, os.path.basename(vid_path))
+        detect_video(yolo, vid_path, output_path=output_path)
     end = timer()
-    print("Processed {} images in {:.1f}sec - {:.1f}FPS".format(len(input_image_paths), end - start, len(input_image_paths) / (end - start)))
-    out_df.to_csv(detection_results_file, index=False)
-
-    yolo.close_session()
+    print("Processed {} videos in {:.1f}sec".format(len(input_video_paths), end - start))
